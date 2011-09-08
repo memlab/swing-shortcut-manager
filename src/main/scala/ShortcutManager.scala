50678d71b3701b8c9667c7b11f6ba857526eacb9
Yuvi Masory <ymasory@gmail.com>
2011-09-08 00:19:16 -0400

    fix column widths

--- src/main/scala/ShortcutManager.scala
+++ src/main/scala/ShortcutManager.scala
@@ -8,8 +8,8 @@ import java.util.EventObject
 import javax.swing.{ Box, BoxLayout }
 import javax.swing.{ AbstractAction, BorderFactory, KeyStroke,
                      ListSelectionModel, SwingUtilities, WindowConstants }
-import javax.swing.{ DefaultCellEditor, JComponent, JFrame, JLabel, JPanel,
-                     JTable, JScrollPane, JTextField, UIManager }
+import javax.swing.{ DefaultCellEditor, JButton, JComponent, JFrame, JLabel,
+                     JPanel, JTable, JScrollPane, JTextField, UIManager }
 import javax.swing.event.TableModelListener
 import javax.swing.table.{ TableCellRenderer, TableModel }
 import javax.swing.text.JTextComponent
@@ -20,33 +20,53 @@ import scala.util.Properties
 
 class ShortcutManager(xactions: List[XAction]) extends JFrame {
 
-  this setSize new Dimension(Scroller.getPreferredSize.getWidth.toInt,
-                             Scroller.getPreferredSize.getHeight.toInt)
+  this setSize(
+    new Dimension(
+      ContentPane.getPreferredSize.getWidth.toInt,
+      ContentPane.getPreferredSize.getHeight.toInt))
   this setDefaultCloseOperation WindowConstants.DO_NOTHING_ON_CLOSE
   this addWindowListener EscapeWindowListener
   this setTitle "Keyboard Shortcuts Manager"
-  this setContentPane Scroller
+  this setContentPane ContentPane
 
-  object Scroller extends JScrollPane {
+  object ContentPane extends JPanel {
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
+    add(Scroller)
+    add(Box.createVerticalBox)
+    add(ResetButtonPanel)
 
-    this setViewportView new ShortcutsTable(xactions)
-    getHorizontalScrollBar setUnitIncrement 15
-    getVerticalScrollBar setUnitIncrement 15
+    object Scroller extends JScrollPane {
 
-    val Exit = "exit"
-    val EscStroke = KeyStroke getKeyStroke (KeyEvent.VK_ESCAPE, 0, false)
-    getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(EscStroke, Exit)
-    getActionMap().put(Exit, new AbstractAction() {
-      val event = new WindowEvent(ShortcutManager.this,
-                                  WindowEvent.WINDOW_CLOSING)
+      this setViewportView new ShortcutsTable(xactions)
+      getHorizontalScrollBar setUnitIncrement 15
+      getVerticalScrollBar setUnitIncrement 15
 
-      override def actionPerformed(e: ActionEvent) =
-        EscapeWindowListener windowClosing event
-    })
+      val Exit = "exit"
+      val EscStroke = KeyStroke getKeyStroke (KeyEvent.VK_ESCAPE, 0, false)
+      getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(EscStroke, Exit)
+      getActionMap().put(Exit, new AbstractAction() {
+        val event = new WindowEvent(ShortcutManager.this,
+                                    WindowEvent.WINDOW_CLOSING)
+
+        override def actionPerformed(e: ActionEvent) =
+          EscapeWindowListener windowClosing event
+      })
+    }
+
+    object ResetButtonPanel extends JPanel {
+      val but = new JButton(new AbstractAction("reset to defaults") {
+        override def actionPerformed(e: ActionEvent) {
+          println("resetting shortcuts to defaults")
+        }
+      })
+      setLayout(new BoxLayout(this, BoxLayout.X_AXIS))
+      add(Box.createHorizontalGlue)
+      add(but)
+      add(Box.createHorizontalGlue)
+    }
   }
 
   object EscapeWindowListener extends WindowAdapter {
-
     override def windowClosing(e: WindowEvent) =
       ShortcutManager.this setVisible false
   }
@@ -54,11 +74,26 @@ class ShortcutManager(xactions: List[XAction]) extends JFrame {
 
 class ShortcutsTable(xactions: List[XAction]) extends RXTable {
 
+  val leftRightPad = 10
+
   this setModel ShortcutsTableModel
   this setSelectionMode ListSelectionModel.SINGLE_SELECTION
   this setFillsViewportHeight true
   this addMouseListener ShortcutsMouseAdapter
   this setSelectAllForEdit true
+
+  for (c <- 0 until getColumnCount) {
+    val col = getColumnModel.getColumn(c)
+    val maxWidth = (0 until getRowCount).map { r =>
+      val renderer = getCellRenderer(r, c)
+      val value = getValueAt(r, c)
+      val cmp =
+        renderer.getTableCellRendererComponent(
+          this, value, false, false, r, c)
+      cmp.getPreferredSize.getWidth.toInt
+    }.max
+    col setMinWidth (maxWidth + 4 + 2 * leftRightPad)
+  }
 
   val header = getTableHeader()
   header setReorderingAllowed false
@@ -70,8 +105,6 @@ class ShortcutsTable(xactions: List[XAction]) extends RXTable {
     new ShortcutsCellEditor(getCellRenderer(rx, cx))
   override def getDefaultEditor(clazz: Class[_]) =
     new ShortcutsCellEditor(getDefaultRenderer(clazz))
-
-  val leftRightPad = 10
 
   object ShortcutsMouseAdapter extends MouseAdapter {
 
