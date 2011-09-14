b426f6f156740f1110212435b30d3994b75dc1be
Yuvi Masory <ymasory@gmail.com>
2011-09-14 02:40:54 -0400

    give java easier access

--- src/main/scala/XAction.scala
+++ src/main/scala/XAction.scala
@@ -5,6 +5,11 @@ case class XAction(val className: String,
                    name: String,
                    tooltip: Option[String],
                    shortcut: Option[Shortcut]) {
+
+  val javaTooltip = tooltip.getOrElse(null)
+  val javaShortcut = shortcut.getOrElse(null)
+  val javaEnum = enum.getOrElse(null)
+
   val id = {
     enum match {
       case Some(str) => List(className, str).mkString("-")
