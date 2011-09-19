6f517141aa34676c5bbc64c13868441d4f5fea17
Yuvi Masory <ymasory@gmail.com>
2011-09-18 20:22:31 -0400

    add license notices

--- src/main/scala/ShortcutManager.scala
+++ src/main/scala/ShortcutManager.scala
@@ -1,3 +1,23 @@
+/*
+This file is part of swing-keyboard-manager
+<https://github.com/memlab/swing-shortcut-manager>, a shortcut manager
+for Java Swing used in Penn TotalRecall
+<http://memory.psych.upenn.edu/TotalRecall>
+
+swing-keyboard-manager is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, version 3 only.
+
+swing-keyboard-manager is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with swing-keyboard-manager.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
 package edu.upenn.psych.memory.shortcutmanager
 
 import java.awt.{ Color, Dimension, Insets }
